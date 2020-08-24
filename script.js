4349028727ef36d94c3e2c0af08083adeb8fab52
Maira Acevedo <m.acevedo53@yahoo.com>
2020-08-23 22:10:56 -0700

    javascriptupdates

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // Assignment Code
-var lowerCase = ["a","b","c","d"];
-var upperCase = ['A','B','C','D'];
+var lowerCase = ["a","b","c","d","e","f","g","h","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = [  
+'A',
+'B',
+'C',
+'D',
+'E',
+'F',
+'G',
+'H',
+'I',
+'J',
+'K',
+'L',
+'M',
+'N',
+'O',
+'P',
+'Q',
+'R',
+'S',
+'T',
+'U',
+'V',
+'W',
+'X',
+'Y',
+'Z',];
 var numbers = [0,1,2,3,4,5,6,7,8,9];
-var spcChrs = ["#","$","*","!",];
+var spcChrs = ["#","$","*","!","@","&","%","^","?"];
 
 
 function generatePassword () {
